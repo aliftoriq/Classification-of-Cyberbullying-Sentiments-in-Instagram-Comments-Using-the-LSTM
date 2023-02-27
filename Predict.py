e7790a24605eb9226047d2ecf8cbfaa6e0a2ac0f
agungdwi <agungparadisafa@gmail.com>
2023-02-27 11:36:26 +0700

    Baseng

---
+++ Predict.py
@@ -0,0 +1,26 @@
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+# Load model
+model = load_model('model.h5')
+
+def predict_class(text):
+    '''Function to predict sentiment class of the passed text'''
+
+    max_fatures = 2000
+    tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+
+    text = ['Meetings: Because none of us is as dumb as all of us.']
+    # vectorizing the tweet by the pre-fitted tokenizer instance
+    text = tokenizer.texts_to_sequences(text)
+    # padding the tweet to have exactly the same shape as `embedding_2` input
+    text = pad_sequences(text, maxlen=28, dtype='int32', value=0)
+    print(text)
+    sentiment = model.predict(text, batch_size=1, verbose=2)[0]
+    if (np.argmax(sentiment) == 0):
+        print("negative")
+    elif (np.argmax(sentiment) == 1):
+        print("positive")
+
+predict_class(['Kmrn termewek2 skr lengket lg duhhh kok labil bgt'])
