924f62446de14c640b5d1e083543574b415065b0
aliftoriq <aliftoriq52@gmail.com>
2023-03-06 12:00:23 +0700

    tested

--- LSTM-1.py
+++ LSTM-1.py
@@ -36,8 +36,8 @@ print("data negative (200 x 2)\t : ", negative.size)
 for idx, row in data.iterrows():
     row[0] = row[0].replace('rt', ' ')
 
-max_fatures = 2000
-tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+max_fatures = [1000, 2000, 3000]
+tokenizer = Tokenizer(num_words=max_fatures[1], split=' ')
 tokenizer.fit_on_texts(data['Instagram Comment Text'].values)
 X = tokenizer.texts_to_sequences(data['Instagram Comment Text'].values)
 X = pad_sequences(X)
@@ -45,11 +45,11 @@ X = pad_sequences(X)
 with open('tokenizer.pickle', 'wb') as handle:
     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-embed_dim = 128
+embed_dim = [64,128]
 lstm_out = 196
 
 model = Sequential()
-model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+model.add(Embedding(max_fatures[1], embed_dim[1],input_length = X.shape[1]))
 model.add(SpatialDropout1D(0.4))
 model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(2,activation='softmax'))
@@ -61,15 +61,9 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random
 print(X_train.shape,Y_train.shape)
 print(X_test.shape,Y_test.shape)
 
-batch_size = 32
-history = model.fit(X_train, Y_train, epochs = 10, batch_size=batch_size, verbose = 2)
+batch_size = [32,64,128]
+history = model.fit(X_train, Y_train, epochs = 10, batch_size=batch_size[2], verbose = 2)
 
-plt.plot(history.history['accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
 
 validation_size = 20
 
@@ -77,10 +71,17 @@ X_validate = X_test[-validation_size:]
 Y_validate = Y_test[-validation_size:]
 X_test = X_test[:-validation_size]
 Y_test = Y_test[:-validation_size]
-score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
+score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size[2])
 print("score: %.2f" % (score))
 print("acc: %.2f" % (acc))
 
+plt.plot(history.history['accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+image.png
 # pos_cnt, neg_cnt, pos_correct, neg_correct = 0, 0, 0, 0
 # for x in range(len(X_validate)):
 
